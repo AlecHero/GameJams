a4d517895c0188fdae89cb5172f6c5e473f724c5
AlecHero <alexander.wittrup@gmail.com>
2025-01-28 18:03:22 +0100

    smolfix

--- cursorWeaponGame/UI.gd
+++ cursorWeaponGame/UI.gd
@@ -25,15 +25,18 @@ func _ready():
 	debug_list = {
 		"fps": func(): return Engine.get_frames_per_second(),
 		"enemy_count": func(): return enemy_handler.get_child_count(),
-		"current_life": func(): return life_ball.current_life,
+		#"current_life": func(): return life_ball.current_life,
 		"": func(): return "",
-		"path_angle": func(): return spear.path_angle,
-		"clamped_mouse_pos": func(): return spear.clamped_mouse_pos,
-		"intersection_point": func(): return spear.intersection_point,
-		"diff": func(): return spear.intersection_point - spear.clamped_mouse_pos,
-		"angle_to_intersect": func(): return ((spear.intersection_point - spear.clamped_mouse_pos).angle()),
-		"direction_change_debuff_t": func(): return spear.direction_change_debuff_t,
-		"spear_collision": func(): return spear.collision_polygon_2d.disabled,
+		"current_weapon": func(): return self.current_weapon_instance.name,
+		"weapon_dmg": func(): return self.current_weapon_instance.current_dmg,
+		"weapon_knockback": func(): return self.current_weapon_instance.current_knockback,
+		
+		#"clamped_mouse_pos": func(): return spear.clamped_mouse_pos,
+		#"intersection_point": func(): return spear.intersection_point,
+		#"diff": func(): return spear.intersection_point - spear.clamped_mouse_pos,
+		#"angle_to_intersect": func(): return ((spear.intersection_point - spear.clamped_mouse_pos).angle()),
+		#"direction_change_debuff_t": func(): return spear.direction_change_debuff_t,
+		#"spear_collision": func(): return spear.collision_polygon_2d.disabled,
 	}
 	
 	for property in debug_list:
@@ -43,7 +46,10 @@ func _ready():
 		debug_container.add_child(sec)
 
 var n = 0
+var current_weapon_instance
 func _process(_delta):
+	current_weapon_instance = cursor_handler.weapon_dict[cursor_handler.current_weapon]
+	
 	health_slider.value = 100 * (life_ball.current_life/life_ball.base_life)
 	speed_slider.value = 100 * spear.velocity_factor
 	
